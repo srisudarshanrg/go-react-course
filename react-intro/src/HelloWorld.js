bdeeee7e1ad5fb010edab772748f233be1122cf8
R Sri Sudarshan <srisudarshanrg@gmail.com>
2025-01-11 19:36:49 +0530

    completed class and functional components in react-intro

---
+++ react-intro/src/HelloWorld.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react';
+
+function HelloWorld() {
+    return (
+        <>
+           <hr />
+           <h1>Hello World</h1> 
+        </>
+        
+        // or we can use fragment
+        // <Fragment>
+        //     <hr />
+        //     <h1>Hello World</h1>
+        // </Fragment>
+    );
+}
+
+export default HelloWorld;
