ee438588976ee3f255b2e614e59cbb23424d071b
R Sri Sudarshan <srisudarshanrg@gmail.com>
2025-01-14 22:04:35 +0530

    started go movies frontend and started with react router basic setup

---
+++ go-movies-front-end/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import Ticket from "../images/movie_tickets.jpg"
+
+function Home() {
+    return (
+        <>
+            <div className="text-center">
+                <h2>Find a movie to watch tonight!</h2>
+                <hr />
+                <img src={Ticket} alt="movie tickets" />
+            </div>
+        </>
+    )
+}
+
+export default Home
---
+++ go-movies-front-end/src/components/Movies.jsx
@@ -0,0 +1,12 @@
+function Movies() {
+    return (
+        <>
+            <div className="text-center">
+                <h2>Movies</h2>
+                <hr />
+            </div>
+        </>
+    )
+}
+
+export default Movies
